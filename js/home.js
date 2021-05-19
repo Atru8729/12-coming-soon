c724b1b4b52ee69f4ed8d55dd134564df21ef0c8
Atru8729 <a.truskauskas1@gmail.com>
2021-05-19 21:18:33 +0300

    socials

--- js/home.js
+++ js/home.js
@@ -1,3 +1,7 @@
 // IMPORT
-
+import { Clock } from "./components/clock/Clock.js";
+import { socials } from "./components/socials/socials.js";
+import { socialsData } from "./data/socialsData.js";
 //EXECUTION
+Clock('#clock_1');
+socials('footer .socials', socialsData);
