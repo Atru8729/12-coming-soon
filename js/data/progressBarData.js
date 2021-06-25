eddf14c9a8a77bd1de94b244a65d4be3c251abfb
Atru8729 <a.truskauskas1@gmail.com>
2021-06-25 18:57:59 +0300

    Progressbar

---
+++ js/data/progressBarData.js
@@ -0,0 +1,16 @@
+const progressBarData = [
+    {
+        title: 'UX Design',
+        value: 90,
+    },
+    {
+        title: 'WEB Design',
+        value: 86,
+    },
+    {
+        title: 'WEB Development',
+        value: 30,
+    },
+];
+
+export { progressBarData };
